6635f21f9b582cd5bf2cd7d38863b1cdcd60c12e
vannberg <vannberg@gmail.com>
2018-11-27 12:41:30 -0500

    Update dataflow.sh

--- dataflow.sh
+++ dataflow.sh
@@ -1,11 +1,11 @@
 #!/bin/bash/
 #Fredrik O. Vannberg, vannberg@gatech.edu, vannberg@gmail.com
 #Georgia Institute of Technology
-#Recommend chmod +x dataflow.sh, ./dataflow.sh <search term 1> <optional search term 2> i.e. ./dataflow.sh metagenomics git
+#Recommend chmod +x dataflow.sh, ./dataflow.sh <search term 1> <optional search term 2>, etc i.e. ./dataflow.sh metagenomics git
 mkdir MochaML
 cd MochaML
 curl -o ./googler https://raw.githubusercontent.com/jarun/googler/v3.7.1/googler
-python3 googler --np -n 100 -C $1 $2 > out.txt
+python3 googler --np -n 100 -C $1 $2 $3 $4 > out.txt
 grep "^https://github.com" out.txt > out_git.txt
 cat out_git.txt | while read line; do GIT_TERMINAL_PROMPT=0 git clone $line; done
 rm out.txt
